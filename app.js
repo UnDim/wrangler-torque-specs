395576993437085d7e7b2d17d64e769ca788f38e
undim <shannonodam@icloud.com>
2025-05-26 00:53:33 -0400

    Added bootstrap

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
